6e8498f106c7dd4778adf797310e381294eb67d4
lmambl <lmambl@mail.ru>
2023-11-03 12:08:26 +0300

    Loader

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-/q/q/q/q/q/q/q/q/q///
-q/q/qq/q/qq/q/q/q/q/q/q/
-//qqqq///qqqq
+const Load = require("./load");
+const inquirer = require("inquirer");
+class Game {
+  constructor() {}
+
+  static helloUser() {
+    Load.startLoad();
+    Load.textInfo();
+    Load.registration();
+  }
+}
+Game.helloUser();
